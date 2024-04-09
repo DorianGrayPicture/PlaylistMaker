366f32c183f01686687dd84c7d2698ad17627de0
DorianGrayPicture <denis123denis2017@yandex.ru>
2024-04-09 16:59:18 +0300

    ImageView fixed

--- app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
@@ -1,10 +1,12 @@
 package com.example.playlistmaker
 
+import android.view.RoundedCorner
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
 class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val trackNameView: TextView
@@ -16,7 +18,7 @@ class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         trackNameView = itemView.findViewById(R.id.trackName)
         artistNameView = itemView.findViewById(R.id.artistName)
         trackTimeView = itemView.findViewById(R.id.trackTime)
-        artworkView = itemView.findViewById(R.id.artWork)
+        artworkView = itemView.findViewById(R.id.artwork)
     }
 
     fun bind(model: Track) {
@@ -28,6 +30,7 @@ class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
             .load(imageUrl)
             .centerCrop()
             .placeholder(R.drawable.ic_track)
+            .transform(RoundedCorners(2))
             .into(artworkView)
     }
 }
